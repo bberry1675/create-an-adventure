a18253b6864687edb71ff8cc4c1d3ceb094ed76e
Brandon Berry <brandon.jn.berry@gmail.com>
2020-03-06 15:20:49 -0500

    added node template

--- routes/node.js
+++ routes/node.js
@@ -19,8 +19,22 @@ router.get('/:node_id', (req,res,next) => {
                 //send the HTMl template the text in the current story node
 
                 //send the HTML template the action values and the corresponding _id values for the next documents
-                res.json(doc);
-                return;
+                doc.populate((err, populated_doc) => {
+                    if(err){
+                        res.status(500).json({error: 'Failed to populate the current node'});
+                        return;
+                    }
+                    else{
+                        res.render('node', {
+                            node_id: populated_doc._id,
+                            story: populated_doc.story,
+                            actions: populated_doc.next
+                        })
+                        return;
+                    }
+                })
+
+                
             }
             else{
                 res.status(404);
