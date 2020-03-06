c28b7c5c4a655b02703b8eb177a242cc29091435
Brandon Berry <brandon.jn.berry@gmail.com>
2020-03-06 13:29:38 -0500

    removed unimplemented css and logs

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ let path = require('path')
 
 //home page to start the adventure
 router.get('/', (req, res) => {
-    console.log(path.join(__dirname,'../views/index.html'));
     res.sendFile(path.join(__dirname,'../views/index.html'));
 });
 
