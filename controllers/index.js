3bb5e1d73217f72f3ac869c74608240b3b4180e0
Brandon Berry <brandon.jn.berry@gmail.com>
2020-03-13 19:43:26 -0400

    updated landing page and footer

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 
 module.exports.get_index = (req,res,next) => {
+    console.log('in here');
     res.render('index',{starting_node: req.app.settings.starting_id});
+    console.log('finished rendering');
     return;
 }
